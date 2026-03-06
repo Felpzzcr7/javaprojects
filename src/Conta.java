1324bba6a199be8dd3f84b77f44ae12564ffcfc3
Felpzzcr7 <felipinholeandro0@gmail.com>
2026-03-06 15:27:52 -0300

    feat: add simple bank account system with deposit and withdraw

---
+++ src/Conta.java
@@ -0,0 +1,70 @@
+public class Conta {
+    private String nome;
+    private double saldo;
+    private double deposito, saque;
+
+
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+
+    public void exibeSaldo(){
+        System.out.println("\nseu saldo atual e de : " + this.saldo+" reais");
+        System.out.println("------------------------------------------------------------\n");
+    }
+    public Conta(String nome, double saldo){
+        this.nome= nome;
+        this.saldo= saldo;
+    }
+
+
+
+
+    public void sacar(){
+        if(saque > saldo){
+            System.out.println("saque nao permitido, saldo insuficiente");
+            System.out.println("------------------------------------------------------------\n");
+        }
+        else {
+            saldo = saldo - saque;
+            System.out.println("seu saque de " + saque +" reais foi realizado com sucesso \n");
+            System.out.println("------------------------------------------------------------\n");
+        }
+
+    }
+
+    public void depositar(){
+        saldo= saldo+deposito;
+        System.out.println("seu deposito de " + deposito +" reais foi realizado com sucesso \n");
+        System.out.println("------------------------------------------------------------\n");
+    }
+}
---
+++ src/Sistemabancario.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Conta p1 = new Conta("silvia", 1000);
+
+    Scanner teclado= new Scanner(System.in);
+
+    System.out.println("BOM DIA "+p1.getNome()+ " o que voce gostaria de fazer? \n aperte: \n 1 para saque \n 2 para deposito \n 3 para ver o seu saldo \n 0 sair");
+    int escolha = teclado.nextInt();
+
+    while ( escolha != 0){
+
+        if(escolha == 1) {
+            System.out.println("quanto voce quer sacar?");
+            double saque = teclado.nextDouble();
+            p1.setSaque(saque);
+            p1.sacar();
+        }
+
+        else if(escolha == 2){
+            System.out.println("quanto voce quer depositar?");
+            double deposito = teclado.nextDouble();
+            p1.setDeposito(deposito);
+            p1.depositar();
+
+        }
+
+        else if(escolha==3) {
+            p1.exibeSaldo();
+        }
+
+        else
+            System.out.println("escolha invalida, selecione uma OPCAO existente\n");
+
+        System.out.println("o que voce quer fazer agora? \n aperte: \n 1 para saque \n 2 para deposito \n 3 para ver o seu saldo \n 0 sair");
+        escolha = teclado.nextInt();
+    }
+}
